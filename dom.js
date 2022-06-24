f8f2c0ae7b147b32e14a41d3323e9572b013c159
Swapnil Katiyar <swapnil240695@gmail.com>
2022-06-25 00:02:52 +0530

    First Commit

--- dom.js
+++ dom.js
@@ -1,7 +1,10 @@
-//Get Element by id
-var headerTitle = document.getElementById('header-title');
-console.log(headerTitle);
-headerTitle.textContent = 'Hello';
-// headerTitle.innertext = 'Goodbye';
-headerTitle.innerHTML = '<h3>Hello</h3>';
-headerTitle.style.border = 'solid 3px #000';
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.backgroundColor = 'yellow';
+for(var i=0; i<items.length; i++) {
+    items[i].style.backgroundColor = '#f4f4f4';
+    items[i].style.fontWeight = 'bold';
+}
+items[2].style.backgroundColor = 'green';
